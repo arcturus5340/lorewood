9021b029c0407e63b56ffbb5f3ea2bbce32a486d
abinba <abror.ruzibayev@gmail.com>
2019-06-17 16:05:34 +0500

    Registration added incompletely

--- app/views.py
+++ app/views.py
@@ -1,14 +1,17 @@
+import json
 import django.http
 import django.shortcuts
 import django.contrib.auth
 import django.contrib.auth.models
 import django.contrib.auth.forms
-import json
+import django_registration.backends.activation.views
+import django_registration.forms
 
 def index(request):
+    regform = django_registration.forms.RegistrationForm
     authform = django.contrib.auth.forms.AuthenticationForm
     authnext = "/"
-    return django.shortcuts.render(request, 'index.html', {'form' : authform, 'next' : authnext})
+    return django.shortcuts.render(request, 'index.html', {'form' : authform, 'next' : authnext, 'regform' : regform })
 
 def login(request):
     if request.method == 'POST':
@@ -29,7 +32,44 @@ def logout(request):
     return django.shortcuts.redirect("/")
 
 def register(request):
-    username = request.POST
-    # Registration function
+    username = request.POST.getlist('username')
+    email = request.POST.getlist('email')
+    password1 = request.POST.getlist('password1')
+    password2 = request.POST.getlist('password2')
+    
+    regform = django_registration.forms.RegistrationForm(request.POST)
+    regview = django_registration.backends.activation.views.RegistrationView()
+    response_data = {}
+
+    registrated = False
+    if(password1 != password2):
+        response_data['result'] = "Пароли не совпадают"
+    else:    
+        try:
+            user = regview.create_inactive_user(regform)
+            registrated = True
+        except ValueError as err:
+            response_data['result'] = "Произошла ошибка при валидации"
+        except AttributeError:
+            registrated = True
+
+    if registrated:
+        response_data['result'] = "Success!" 
+
+    return django.http.HttpResponse(json.dumps(response_data), content_type="application/json")
+
+    # response_data = {}
+    # if password1[0] == password2[0]:
+    #     user = django.contrib.auth.models.User.objects.create_user(username[0], email[0], password1[0])
+    # else:
+    #     response_data['result'] = "Ошибка! Пароли не совпадают."
+
+    # if user is not None:
+    #     response_data['result'] = 'Success!'
+    # else:
+    #     response_data['result'] = user
+
+    # return django.http.HttpResponse(json.dumps(response_data), content_type="application/json")
+    # # Registration function
 
 
