ed6974ec7c3429c1d78ae46cfe3575295379a7b6
abinba <abror.ruzibayev@gmail.com>
2019-06-18 04:20:39 +0500

    Refactored *.html by arcturus5340

--- app/urls.py
+++ app/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('register/', views.register, name='register'),
+    path('advertising/', views.advertising, name='advertising'),
+    path('donations/', views.donations, name='donations'),
+    path('info/', views.info, name='info'),
+    path('regulations/', views.regulations, name='regulations'),
+    path('rightholder/', views.rightholder, name='rightholder'),
+    path('nazvanie-zapisi-8-nazvanie-zapisi-8/', views.nazvanie_zapisi_8_nazvanie_zapisi_8, name='advertising'),
 ]
