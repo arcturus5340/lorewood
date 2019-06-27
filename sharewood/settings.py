8a40e3c5b0a3e4165ee0d2f3d0205fe1e442c92e
arcturus5340 <arcturus5340@gmail.com>
2019-06-27 16:03:44 +0500

    Migration from SQLite3 to postgres

--- sharewood/settings.py
+++ sharewood/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'sharewood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db',
+        'USER' : 'arcturus',
+        'PASSWORD' : 'password',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
     }
 }
 
