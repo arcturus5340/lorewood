d284b8fe52a1ba144d39aee9802d1980d8afd383
arcturus5340 <arcturus5340@gmail.com>
2020-07-01 15:51:28 +0200

    update popular posts every day

--- app/apps.py
+++ app/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class AppConfig(AppConfig):
     name = 'app'
+
+    def ready(self):
+        from app import popular_records_gen
+        popular_records_gen.start()
