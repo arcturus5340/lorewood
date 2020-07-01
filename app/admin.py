f89acd8bb273f60202cedb865e7e6aa2e84b7410
arcturus5340 <arcturus5340@gmail.com>
2020-07-01 16:59:18 +0200

    refactor admin.py
    Improved code readability

--- app/admin.py
+++ app/admin.py
@@ -1,46 +1,94 @@
-from django.contrib import admin
-from app.models import Records
-from app.models import Media
-from app.models import Premium
-from app.models import Profile
 from django import forms
+from django.contrib import admin
+
+from app.models import Media, Records, Premium, Profile
+
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
 import social_django.models
+
+
+class MediaAdmin(admin.ModelAdmin):
+	list_display = (
+		'record',
+		'title',
+		*['file{}'.format(i) for i in range(1, 26)],
+	)
+	list_filter = search_fields = (
+		'record',
+		'title',
+	)
+
+
+class PremiumAdmin(admin.ModelAdmin):
+	list_display = (
+		'id',
+		'premium_cost',
+	)
+
+
+class ProfileAdmin(admin.ModelAdmin):
+	list_display = (
+		'user',
+		'avatar',
+		'bio',
+		'balance',
+		'is_premium',
+	)
+	search_fields = (
+		'user',
+		'bio',
+	)
+
 
 class RecordsAdminForm(forms.ModelForm):
 	text = forms.CharField(widget=CKEditorUploadingWidget())
 	description = forms.CharField(widget=CKEditorUploadingWidget())
 	includes = forms.CharField(widget=CKEditorUploadingWidget())
 
+
 class RecordsAdmin(admin.ModelAdmin):
 	form = RecordsAdminForm
-	list_display = ('title', 'author', 'description', 'includes', 'text', 'price', 'date', 'tags', 
-		'pre_video', 'main_pic')
-	list_filter = ('date', 'tags')
-	search_fields = ('title', 'author', 'description', 'date', 'tags', 'text')
+	list_display = (
+		'title',
+		'author',
+		'description',
+		'includes',
+		'text',
+		'price',
+		'date',
+		'tags',
+		'pre_video',
+		'main_pic',
+	)
+	list_filter = (
+		'date',
+		'tags',
+	)
+	search_fields = (
+		'title',
+		'author',
+		'description',
+		'date',
+		'tags',
+		'text',
+	)
+	exclude = (
+		'rating',
+		'best_rating',
+		'worst_rating',
+		'rating_count',
+		'rating_sum',
+		'rated_users',
+		'comments_count',
+		'sales',
+	)
 
-	exclude = ('rating', 'best_rating', 'worst_rating', 'rating_count', 'rating_sum', 'rated_users', 'comments_count', 'sales')
-
-class MediaAdmin(admin.ModelAdmin):
-	list_display = ('record', 'title', 'file1', 'file2', 'file3', 'file4', 'file5', 'file6'
-		, 'file7', 'file8', 'file9', 'file10', 'file11', 'file12', 'file13', 'file14', 'file15', 'file16',
-        'file17', 'file18', 'file19', 'file20', 'file21', 'file22', 'file23', 'file24', 'file25')
-	list_filter = ('record', 'title')
-	search_fields = ('record', 'title')
-
-class PremiumAdmin(admin.ModelAdmin):
-	list_display = ('id', 'premium_cost')
-
-class ProfileAdmin(admin.ModelAdmin):
-	list_display = ('user', 'avatar', 'bio', 'balance', 'is_premium')
-	search_fields = ('user', 'bio')
 
 admin.site.site_header = "Sharewood"
-admin.site.register(Records, RecordsAdmin)
-admin.site.register(Profile, ProfileAdmin)
-admin.site.register(Premium, PremiumAdmin)
 admin.site.register(Media, MediaAdmin)
+admin.site.register(Premium, PremiumAdmin)
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Records, RecordsAdmin)
 admin.site.unregister(social_django.models.Association)
 admin.site.unregister(social_django.models.UserSocialAuth)
 admin.site.unregister(social_django.models.Nonce)
-
