3dc6b842e812d402515b9f76076989b44e66eef4
arcturus5340 <arcturus5340@gmail.com>
2020-07-21 19:08:08 +0200

    cleanup urls.py app file

--- app/urls.py
+++ app/urls.py
@@ -1,38 +1,35 @@
 from django.urls import path
-from .views import api, auth, cabinet, menu
+from .views import confirmation, menu, statistics, auth, cabinet
 
 urlpatterns = [
-    path('', menu.index, name='index'),
-    path('login/', auth.login, name='login'),
-    path('remember/', auth.remember, name='remember'),
+    path('', menu.index),
+    path('info/', menu.info),
+    path('regulations/', menu.regulations),
+    path('advertising/', menu.advertising),
+    path('rightholder/', menu.rightholder),
+    path('donations/', menu.donations),
 
-    path('logout/', auth.logout, name='logout'),
-    path('register/', auth.register, name='register'),
-    path('advertising/', menu.advertising, name='advertising'),
-    path('donations/', menu.donations, name='donations'),
-    path('info/', menu.info, name='info'),
-    path('regulations/', menu.regulations, name='regulations'),
-    path('rightholder/', menu.rightholder, name='rightholder'),
+    path('r<int:record_id>/', menu.record),
+    path('r<int:record_id>/buy/', menu.buy),
 
-    path('user/<str:username>/activate/<str:activation_key>', auth.activate_account, name='activate_account'),
-    path('user/<str:username>/verificate/<str:activation_key>', auth.verificate_login, name='verificate_login'),
-    path('save-personal-data/', cabinet.save_personal_data, name="save_personal_data"),
-    path('user/<str:username>/cabinet/<str:list>', cabinet.cabinet, name="cabinet"),
-    path('user/<str:username>/remember/<str:activation_key>', cabinet.password_change_view, name='password_change_view'),
-    path('user/<str:username>/change-password/', cabinet.change_password, name="change_password"),
-    path('change-cabinet-password/', cabinet.change_cabinet_password, name="change_cabinet_password"),
-    path('change-email/', auth.change_email, name="change_email"),
-    path('user/<str:username>/change-email/<str:activation_key>',
-         cabinet.change_email_confirm,
-         name="change_email_confirm"),
+    path('tag/<str:tag>/', menu.records_by_tags),
+    path('search/', menu.search),
+    path('statistics/', statistics.statistics),
 
-    path('r<int:record_id>/', menu.record, name='record'),
-    path('tag/<str:tag>/', menu.records_by_tags, name='records_by_text'),
-    path('search', menu.search, name='search'),
-    path('r<int:record_id>/buy/', menu.buy, name='buy'),
-    path('buy_premium/', cabinet.buy_premium, name='buy_premium'),
-    path('two_verif_on/', cabinet.two_verif_on, name='two_verif_on'),
-    path('two_verif_off/', cabinet.two_verif_off, name='two_verif_off'),
+    path('login/', auth.login),
+    path('remember/', auth.remember),
+    path('change-email/', auth.change_email),
+    path('register/', auth.register),
+    path('logout/', auth.logout),
 
-    path('statistics/', api.statistics),
+    path('user/<str:username>/activate/<str:activation_key>', confirmation.activate_setting),
+
+    path('user/<str:username>/cabinet', cabinet.cabinet),
+    path('save-personal-data/', cabinet.save_personal_data),
+    path('user/<str:username>/change-password/', cabinet.change_password),
+    path('change-password/', cabinet.change_password),
+
+    path('buy_premium/', cabinet.buy_premium),
+    path('two_verif_on/', cabinet.two_verif_on),
+    path('two_verif_off/', cabinet.two_verif_off),
 ]
