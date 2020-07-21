68a8b00c2aef031361cd1e3a7d33ebf286afd57a
arcturus5340 <arcturus5340@gmail.com>
2020-07-21 16:27:16 +0200

    set specific table names
    This is to comply with the PEP, which provides Camelcase
    for class names. If you do not specify the table names
    explicitly, they will merge, because django does not
    preserve the case of the table class names

--- app/models.py
+++ app/models.py
@@ -1,20 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import gettext as _
 
-from django.contrib.auth.models import User
 
-
-class Tags(models.Model):
+class Tag(models.Model):
     id = models.AutoField(primary_key=True)
     tag = models.CharField(max_length=32, unique=True)
 
+    class Meta:
+        db_table = 'app_tags'
+        verbose_name = _("Tag")
+        verbose_name_plural = _("Tags")
 
-class Records(models.Model):
+
+class Record(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=128)
     content = models.TextField()
     description = models.CharField(max_length=256)
-    tags = models.ManyToManyField(Tags)
+    tags = models.ManyToManyField(Tag)
     main_pic = models.FileField(upload_to='record_src/')
     pre_video = models.FileField(upload_to='record_src/')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -28,62 +33,92 @@ class Records(models.Model):
     sales = models.IntegerField(default=0)
 
     class Meta:
-        verbose_name = "Запись" 
-        verbose_name_plural = "Записи"
+        db_table = 'app_records'
+        verbose_name = _("Record")
+        verbose_name_plural = _("Records")
 
 
-class Provided_Users(models.Model):
+class ProvidedUser(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    record = models.ForeignKey(Records, on_delete=models.CASCADE)
-
-
-class Rated_Users(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    record = models.ForeignKey(Records, on_delete=models.CASCADE)
+    record = models.ForeignKey(Record, on_delete=models.CASCADE)
 
     class Meta:
-        unique_together = ('user', 'record',)
+        db_table = 'app_provided_users'
+        unique_together = ['user', 'record']
+        verbose_name = _("Provided User")
+        verbose_name_plural = _("Provided Users")
 
 
-class Headers(models.Model):
+class RatedUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    record = models.ForeignKey(Record, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'app_rated_users'
+        unique_together = ['user', 'record']
+        verbose_name = _("Rated User")
+        verbose_name_plural = _("Rated Users")
+
+
+class Header(models.Model):
     id = models.AutoField(primary_key=True)
-    record = models.ForeignKey(Records, on_delete=models.CASCADE)
+    record = models.ForeignKey(Record, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     _order = models.IntegerField(default=0)
 
+    class Meta:
+        db_table = 'app_headers'
+        verbose_name = _("Header")
+        verbose_name_plural = _("Headers")
 
-class Files(models.Model):
+
+class File(models.Model):
     id = models.AutoField(primary_key=True)
-    header = models.ForeignKey(Headers, on_delete=models.CASCADE)
+    header = models.ForeignKey(Header, on_delete=models.CASCADE)
     src = models.FileField(upload_to='record_src/')
     _order = models.IntegerField(default=0)
 
     class Meta:
-        verbose_name = "Материал" 
-        verbose_name_plural = "Материалы"
+        db_table = 'app_files'
+        verbose_name = _("File")
+        verbose_name_plural = _("Files")
 
 
-class Comments(models.Model):
+class Comment(models.Model):
     id = models.AutoField(primary_key=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     date = models.DateTimeField(default=timezone.now())
-    record = models.ForeignKey(Records, on_delete=models.CASCADE)
+    record = models.ForeignKey(Record, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'app_comments'
+        verbose_name = _("Comment")
+        verbose_name_plural = _("Comments")
 
 
 class Revenue(models.Model):
     date = models.DateField(default=timezone.now())
     income = models.IntegerField(default=0)
 
+    class Meta:
+        db_table = 'app_revenue'
+        verbose_name = _("Revenue")
 
-class Activation(models.Model):
+
+class ActivationKey(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     activation_key = models.TextField(unique=True)
-    is_registration = models.BooleanField(default=False)
-    is_2stepverif = models.BooleanField(default=False)
-    is_remember = models.BooleanField(default=False)
+    is_2step_verification = models.BooleanField(default=False)
     is_email_change = models.BooleanField(default=False)
+    is_registration = models.BooleanField(default=False)
+    is_remember = models.BooleanField(default=False)
     new_email = models.CharField(max_length=254, null=True, blank=True)
+
+    class Meta:
+        db_table = 'app_activation_keys'
+        verbose_name = _("Activation Key")
+        verbose_name_plural = _("Activation Keys")
 
 
 class Profile(models.Model):
@@ -92,13 +127,19 @@ class Profile(models.Model):
     bio = models.TextField(max_length=500, null=True, blank=True)
     balance = models.IntegerField(default=0)
     is_premium = models.BooleanField(default=False)
-    has_2stepverif = models.BooleanField(default=False)
     is_verified = models.BooleanField(default=False)
+    has_2step_verification = models.BooleanField(default=False)
 
     class Meta:
-        verbose_name = "Профиль" 
-        verbose_name_plural = "Профили"
+        db_table = 'app_profile'
+        verbose_name = _("Profile")
+        verbose_name_plural = _("Profiles")
 
-class Global_Settings(models.Model):
+
+class GlobalSettings(models.Model):
     setting = models.TextField(unique=True)
     value = models.IntegerField()
+
+    class Meta:
+        db_table = 'app_global_settings'
+        verbose_name = _("Global Settings")
