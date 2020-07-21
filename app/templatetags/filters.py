65f5211e7f37bafafa28336fe197ce498993a51f
arcturus5340 <arcturus5340@gmail.com>
2020-07-21 19:06:03 +0200

    remove unused split filter

--- app/templatetags/filters.py
+++ app/templatetags/filters.py
@@ -4,11 +4,6 @@ import django.template
 register = django.template.Library()
 
 
-@register.filter(name='split')
-def split_filter(value, arg):
-    return value.split(arg)
-
-
 @register.filter(name='strftime')
 def strftime(date, template):
     return date.strftime(template)
